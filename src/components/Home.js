d8c2a0c125804b140f73bd348fa9930fcab6507c
sachin <ysachin511@gmail.com>
2022-12-10 00:15:04 +0530

    change

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import { serverUrl,clientUrl } from "../config";
 
 const Home=()=>{
     const [user, setUser] = useState(null);
+    const [welcomePage,setWelcomePage]=useState(null)
     const logout = () => {
         window.open(`${serverUrl}/auth/logout`, "_self");
         
@@ -28,33 +29,37 @@ const Home=()=>{
             .then((response) => {
               console.log('user',response)
               setUser(response.user);
+              setWelcomePage(<div>
+                <h1>Welcome {response.user.displayName}</h1>
+                <div>
+                  <h3>Details</h3>
+                  <div>
+                    <img src={response.user.photos[0].value} alt="pic"></img>
+                  </div>
+                </div>
+                <div className="logout-btn">
+            <Button variant="danger" onClick={logout}>
+              Log Out
+            </Button>
+          </div>
+              </div>)
             })
             .catch((err) => {
+              setWelcomePage(<div>
+                <h1>Login failed</h1>
+              </div>)
+            console.log(err);
               console.log(err);
             });
         };
         getUser(); 
       }, []);
     return <>
-        <div className="home">
-        {user ? (<div>
-            <h1>Welcome {user.displayName} !!!</h1>
-            <div>
-                <h3>Details</h3>
-                <div >
-                    <img src={user.photos[0].value} alt="pic" className="pic"></img>
-                </div>
-                
-            </div>
-        </div>) : (<div>
-            <h1>Login failed</h1>
-        </div>)}
-        
-
-        <div className="logout-btn">
-        <Button  variant="danger" onClick={logout}>Log Out</Button>
-        </div>
-        </div>
+       <div className="home">
+        {
+          welcomePage
+        } 
+      </div>
 
     </>
 }
