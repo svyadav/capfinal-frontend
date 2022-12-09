cd49b34067010ad9f729532a18a3bdb2001bc0e6
sachin <ysachin511@gmail.com>
2022-12-09 23:37:17 +0530

    change

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
-// const serverUrl = 'http://localhost:5000'
+
 const serverUrl = 'http://localhost:5000'
 const clientUrl = 'http://localhost:3000'
+
 module.exports = {
     serverUrl,
     clientUrl
